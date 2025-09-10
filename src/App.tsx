58ab4b777a15136b6e36e09292f19c5fe975dc25
vijaymahato2805 <vijaymahato2805@gmail.com>
2025-09-10 21:04:00 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 // src/App.tsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/HomePage';
-import SalonDetail from './pages/SalonDetailPage';
-import Login from './pages/LoginPage';
-import Booking from './pages/BookingPage';
-import AuthGuard from './components/AuthGuard';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/HomePage";
+import SalonDetail from "./pages/SalonDetailPage";
+import Login from "./pages/LoginPage";
+import Booking from "./pages/BookingPage";
+import MyBookingsPage from "./pages/MyBookingsPage"; // 👈 import it
+import AuthGuard from "./components/AuthGuard";
 
 function App() {
   return (
@@ -14,7 +15,22 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Home />} />
           <Route path="/salons/:id" element={<SalonDetail />} />
-          <Route path="/book/:salonId" element={<AuthGuard><Booking /></AuthGuard>} />
+          <Route
+            path="/book/:salonId"
+            element={
+              <AuthGuard>
+                <Booking />
+              </AuthGuard>
+            }
+          />
+          <Route
+            path="/my-bookings"
+            element={
+              <AuthGuard>
+                <MyBookingsPage />
+              </AuthGuard>
+            }
+          />
         </Routes>
       </div>
     </Router>
